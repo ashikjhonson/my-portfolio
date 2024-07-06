3a70776c366c25895bbece35e10262dde691b166
Ashik Jhonson <ashikjhonson@gmail.com>
2024-07-06 14:05:58 +0530

    Hero & About

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import Hero from "./components/Hero";
+// import Footer from "./components/Footer";
+import About from "./components/About";
+// import Project from "./components/Project";
+// import Hero from "./components/Hero";
+// import Certificate from "./components/Certificate";
+
+function App() {
+  return (
+    <div className="App bg-gray-100 dark:bg-[#0e1525]">
+      <Hero />
+      <About />
+    </div>
+  );
+}
+
+export default App;
